25464643558280e88c57acb065226a271c2b7e42
mdanish31 <114271916+mdanish31@users.noreply.github.com>
2026-07-04 23:58:12 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  basePath: "/enovoweb",
+  assetPrefix: "/enovoweb/",
+  trailingSlash: true,
+
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
